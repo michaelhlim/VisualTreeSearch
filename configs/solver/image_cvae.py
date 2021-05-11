74db33803c3d5ce384b5c5795055519274252f64
sdeglurkar <sampada_deglurkar@berkeley.edu>
2021-05-11 16:30:48 -0700

    CGAN for stanford dataset

--- configs/solver/image_cvae.py
+++ configs/solver/image_cvae.py
@@ -4,19 +4,23 @@ class Image_CVAE_Params():
         self.latent_dim = 64
         self.dim_conditional_var = 3
         self.img_size = 32 
-        self.num_training_steps = int(2e3) #int(50e3)
-        self.data_path = "/home/ext_drive/sampada_deglurkar/vae_stanford/training/rgbs/*"
-        self.save_path = "image_cvae_calib_normlr1e-3.pth"
-        self.training_loss_path = "image_cvae_training_losses_calib_normlr1e-3"
-        self.test_data_path = "/home/ext_drive/sampada_deglurkar/vae_stanford/testing/rgbs/*"
-        self.test_model_path = "/home/sampada_deglurkar/VisualTreeSearch/src/solvers/image_cvae_calib_normlr1e-3.pth"
-        self.test_output_path = "output_test_calib_normlr1e-3.png"
         self.batch_size = 64
-        self.lr = 1e-3 #5e-5
+        self.num_training_data = 7000
+        self.num_epochs = 100
+        self.num_training_steps = int((self.num_training_data/self.batch_size) * self.num_epochs)  
+        self.data_path = "/home/ext_drive/sampada_deglurkar/vae_stanford/training_hard/rgbs/*"
+        self.save_path = "image_cvae_hard.pth"
+        self.training_loss_path = "image_cvae_hard"
+        self.test_data_path = "/home/ext_drive/sampada_deglurkar/vae_stanford/testing_hard/rgbs/*"
+        self.test_model_path = "/home/sampada_deglurkar/VisualTreeSearch/src/solvers/image_cvae_hard.pth"
+        self.test_output_path = "test_output_hard"
+        self.test_true_path = "test_true_hard"
+        self.lr = 1e-3 
         self.beta = 1
         self.device = 'cuda:1'
         self.calibration = True
+        self.normalization = False
 
-        # Epoch size: 2000/batch_size = 31.25 steps
-        # 1500 steps is about 50 epochs
+        self.train = False
+
 
--- src/solvers/image_cgan_training.py
+++ src/solvers/image_cgan_training.py
@@ -7,49 +7,56 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from configs.solver.image_cvae import *
-from src.solvers.image_cvae import *
+from configs.solver.image_cgan import *
+from src.solvers.image_cgan import *
 
-image_cvae_params = Image_CVAE_Params()
-
-torch.backends.cudnn.enabled = False
+image_cgan_params = Image_CGAN_Params()
 
 
-class VAETrain:
+class GANTrain():
 
     def __init__(self):
-        self.device = torch.device(image_cvae_params.device)
+        self.device = torch.device(image_cgan_params.device)
 
-        self.in_channels = image_cvae_params.in_channels
-        self.dim_conditional_var = image_cvae_params.dim_conditional_var
-        self.latent_dim = image_cvae_params.latent_dim
-        self.img_size = image_cvae_params.img_size
-        self.calibration = image_cvae_params.calibration
-        self.normalization = image_cvae_params.normalization
+        self.state_enc_out_dim = image_cgan_params.state_enc_out_dim
+        self.dim_state = image_cgan_params.dim_state
+        self.in_channels = image_cgan_params.in_channels
 
-        args = {'in_channels': self.in_channels, 'dim_conditional_var': self.dim_conditional_var,
-                'latent_dim': self.latent_dim, 'img_size': self.img_size, 'calibration': self.calibration, 
-                'device': self.device}
-        self.model = ConditionalVAE(args)
+        args = {'state_enc_out_dim': self.state_enc_out_dim, 'dim_state': self.dim_state, 
+                'in_channels': self.in_channels, "device": self.device}
 
-        self.num_training_steps = image_cvae_params.num_training_steps
-        self.data_path = image_cvae_params.data_path
-        self.test_data_path = image_cvae_params.test_data_path
-        self.batch_size = image_cvae_params.batch_size
-        self.beta = image_cvae_params.beta
-        self.lr = image_cvae_params.lr
+        self.op_model = ObsPredictorNetwork(args)
+        self.m_model = MeasureNetwork(args)
 
+        self.num_training_steps = image_cgan_params.num_training_steps
+        self.data_path = image_cgan_params.data_path
+        self.test_data_path = image_cgan_params.test_data_path
+        self.batch_size = image_cgan_params.batch_size
+        self.lr = image_cgan_params.lr
+        self.normalization = image_cgan_params.normalization
+
+        # self.m_optimizer = torch.optim.Adam(self.m_model.parameters(), lr=self.lr)
+        # self.op_optimizer = torch.optim.Adam(self.op_model.parameters(), lr=self.lr)
+        self.m_optimizer = torch.optim.SGD(self.m_model.parameters(), lr=self.lr)
+        self.op_optimizer = torch.optim.SGD(self.op_model.parameters(), lr=self.lr)
+
+        self.img_size = image_cgan_params.img_size
         self.data_files = glob.glob(self.data_path)
         self.testing_data_files = glob.glob(self.test_data_path)
     
-        self.save_path = image_cvae_params.save_path
-        self.training_loss_path = image_cvae_params.training_loss_path
-        self.test_model_path = image_cvae_params.test_model_path
-        self.test_output_path = image_cvae_params.test_output_path
-        self.test_true_path = image_cvae_params.test_true_path
+        self.op_save_path = image_cgan_params.op_save_path
+        self.m_save_path = image_cgan_params.m_save_path
+        self.op_training_loss_path = image_cgan_params.op_training_loss_path
+        self.m_training_loss_path = image_cgan_params.m_training_loss_path
+
+        self.op_test_model_path = image_cgan_params.op_test_model_path
+        self.m_test_model_path = image_cgan_params.m_test_model_path
+
+        self.test_output_path = image_cgan_params.test_output_path
+        self.test_true_path = image_cgan_params.test_true_path
 
         self.rmean, self.gmean, self.bmean, self.rstd, self.gstd, self.bstd = self.preprocess_data()
-
+    
 
     def preprocess_data(self):
         # For normalizing the images - per channel mean and std
@@ -75,15 +82,7 @@ class VAETrain:
         
         return rmean, gmean, bmean, rstd, gstd, bstd
 
-        #     src = src/len(self.data_files)
-        #     if i == 0:
-        #         mean_image = src
-        #     else:
-        #         mean_image = src + mean_image
-        
-        # return mean_image
-
-
+    
     def get_training_batch(self, batch_size):
         states = []
         images = []
@@ -141,49 +140,87 @@ class VAETrain:
             state = np.array([[np.round(float(elem), 3) for elem in splits[-3:]]])
             states.append(state)
         
-        return np.array(states), np.array(images)    
+        return np.array(states), np.array(images) 
 
-    
+
     def train(self):
-        optimizer = self.model.configure_optimizers(self.lr)
+        MSE_criterion = nn.MSELoss()
+        BCE_criterion = nn.BCELoss()
+
         print_freq = 50
-        self.training_losses = []
+        update_freq = 10
+        self.op_training_losses = []
+        self.m_training_losses = []
 
         t0 = time.time()
         for step in range(self.num_training_steps):
             states, images = self.get_training_batch(self.batch_size)
             states = torch.from_numpy(states).float().to(self.device)
+            states = states.squeeze(1)
             images = torch.from_numpy(images).float().to(self.device)
             images = images.permute(0, 3, 1, 2)  # [batch_size, in_channels, 32, 32]
 
-            optimizer.zero_grad()
-            [recons, input, mu, log_var] = self.model.forward(images, states)
-            args = [recons, input, mu, log_var]
-            loss_dict = self.model.loss_function(self.beta, *args)
-            loss = loss_dict['loss']
-            recon_loss = loss_dict['Reconstruction_Loss']
-            kl_loss = loss_dict['KLD']
-            loss.backward()
-            optimizer.step()
+            # ----------------------------
+            #  Train Observation Predictor
+            # ----------------------------
+            noise = torch.randn_like(states)
+            noisy_states = states #+ noise
+            self.op_optimizer.zero_grad()
+            obs_predicted = self.op_model(noisy_states)  # (batch_size, in_channels, 32,)
+            OP_loss = 0
+
+            fake_logit = self.m_model(noisy_states, obs_predicted)  # (batch_size, 1)
+            real_target = torch.ones_like(fake_logit) * 0.9
+            OP_loss += BCE_criterion(fake_logit, real_target)
+
+            OP_loss.backward()
+            self.op_optimizer.step()
+
+            # ------------------------
+            #  Train Discriminator
+            # ------------------------
+            if step % update_freq == 0:
+                self.m_optimizer.zero_grad()
+
+                noise = torch.randn_like(states)
+                noisy_states = states + noise
+                fake_logit = self.m_model(noisy_states, obs_predicted.detach())  # (batch_size, 1)
+                fake_target = torch.zeros_like(fake_logit)  # (batch_size, 1)
+                fake_loss = BCE_criterion(fake_logit, fake_target)
+
+                real_logit = self.m_model(noisy_states, images)  # (batch_size, 1)
+                real_target = torch.ones_like(real_logit)
+                real_loss = BCE_criterion(real_logit, real_target)
+                OM_loss = real_loss + fake_loss
+                OM_loss.backward()
+                self.m_optimizer.step()
+
 
             if step % print_freq == 0:
-                print(step, loss.item(), kl_loss.item(), recon_loss.item())
-                self.training_losses.append((step, loss.item()))
+                print(step, OP_loss.item(), OM_loss.item())
+                self.op_training_losses.append((step, OP_loss.item()))
+                self.m_training_losses.append((step, OM_loss.item()))
+
         
         t1 = time.time()
-        torch.save(self.model.state_dict(), self.save_path)
-
+        torch.save(self.op_model.state_dict(), self.op_save_path)
+        torch.save(self.m_model.state_dict(), self.m_save_path)
+        
+        steps = [loss[0] for loss in self.op_training_losses]
+        op_losses = [loss[1] for loss in self.op_training_losses]
+        m_losses = [loss[1] for loss in self.m_training_losses]
         plt.figure()
-        steps = [loss[0] for loss in self.training_losses]
-        losses = [loss[1] for loss in self.training_losses]
-        plt.plot(steps, losses)
-        plt.savefig(self.training_loss_path) 
+        plt.plot(steps, op_losses)
+        plt.savefig(self.op_training_loss_path) 
+        plt.figure()
+        plt.plot(steps, m_losses)
+        plt.savefig(self.m_training_loss_path) 
 
         print("TRAINING TIME", t1 - t0)
-    
+
 
     def test(self):
-        self.model.load_state_dict(torch.load(self.test_model_path))
+        self.op_model.load_state_dict(torch.load(self.op_test_model_path))
         num_tests = 1
         
         states, images = self.get_testing_batch(num_tests)
@@ -193,7 +230,7 @@ class VAETrain:
         states = states.detach()
         images = images.detach()
 
-        output = self.model.sample(1, states[0])  # [1, in_channels, 32, 32]
+        output = self.op_model(states[0])  # [1, in_channels, 32, 32]
         output = output.permute(0, 2, 3, 1).squeeze(0)  # [32, 32, in_channels]
         output = output.detach().cpu().numpy()        
         #output = (output + 68./100) * 100.
@@ -224,10 +261,9 @@ class VAETrain:
         cv2.imwrite(self.test_true_path + state_str, original)
 
 
-vae_train = VAETrain()
-if image_cvae_params.train:
-    vae_train.train()
+gan_train = GANTrain()
+if image_cgan_params.train:
+    gan_train.train()
 else:
-    vae_train.test()
+    gan_train.test()
 
-        
